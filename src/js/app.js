b9f0cbed2c969475ef1a13d5aabe14593ab9f0fd
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-01-13 16:25:34 +0300

    ...

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,49 @@
+/* eslint-disable max-len */
+/* eslint-disable max-classes-per-file */
+/* eslint-disable no-return-assign */
 export default class Team {
-    constructor() {
-        
-    }
+  constructor() {
+    this.characters = null; // Зачем создаётся сущность с null?
+  }
+
+  [Symbol.iterator]() {
+    let index = 0;
+    const { characters } = this;
+    const { size } = characters;
+
+    return {
+      next() {
+        if (index < size) {
+          return {
+            done: false,
+            value: [...characters][index += 1], // Зачем тут используется ... , которые разделяют массив на элементы, если потом это дело все равно заворачивается в масив?!
+          };
+        }
+
+        return {
+          done: true,
+        };
+      },
+    };
+  }
+
+  insertCharacters(arr) { // Для чего создаётся этот массив?
+    this.characters = new Set(arr);
+  }
+}
+
+export class Person extends Team {
+  constructor(name) {
+    super();
+    this.name = name;
+  }
+}
+
+const team = new Team();
+const dima = new Person('dima');
+const alex = new Person('alex');
+const misha = new Person('misha');
+
+for (const prop of [dima, alex, misha]) {
+  console.log(prop);
 }
